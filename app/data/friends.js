5162d1048963d677d46bbd0c4d95a53972183084
code3lue <alan.delez@gmail.com>
2017-04-23 05:23:31 -0400

    major push

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [
+    {
+        "name": "Peter",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/c/c2/Peter_Griffin.png",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+        ]
+    },
+]
